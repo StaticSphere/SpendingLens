4c9d000002c3a9411edc624108343c13bf66ca70
Jamie Nordmeyer <jamie@staticsphere.com>
2018-06-08 14:17:26 -0700

    Added Material, wrapped shell with theme provider

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 import configureStore from './configureStore';
 import Shell from './Shell';
+import lightTheme from './themes/lightTheme';
 
 const Main = () => {
     const { store } = configureStore();
 
     return (
         <Provider store={store}>
-            <Shell />
+            <MuiThemeProvider muiTheme={lightTheme}>
+                <Shell />
+            </MuiThemeProvider>
         </Provider>
     );
 };
---
+++ src/renderer/themes/lightTheme.ts
@@ -0,0 +1,12 @@
+import { getMuiTheme } from 'material-ui/styles';
+import { white } from 'material-ui/styles/colors';
+
+export default getMuiTheme({
+    palette: {
+        primary1Color: "#ff9800"
+    },
+    toolbar: {
+        height: 64,
+        backgroundColor: white
+    }
+});
